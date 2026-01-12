7355ad50af5994c4029a25d4a115be3de658cdec
ThureinS <94038134+ThureinS@users.noreply.github.com>
2026-01-13 01:38:21 +0630

    log component style change

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Log = ({ turns }) => {
   return (
-    <div>
+    <ol id="log">
       {turns.map((turn) => (
         <li key={`${turn.square.row}${turn.square.col}`}>
           {turn.player} selected {turn.square.row}, {turn.square.col}
         </li>
       ))}
-    </div>
+    </ol>
   );
 };
 
